9904fc30c1af0e238d490c77c3b18106318139c4
David-Kirsch <davidjkirsch@gmail.com>
2020-10-16 23:15:44 -0400

    changed seeds

--- app/models/apartment.rb
+++ app/models/apartment.rb
@@ -2,13 +2,13 @@ class Apartment < ApplicationRecord
     has_many :comments, dependent: :delete_all
     has_many :users, through: :comments
 
-    # validates :street_number, uniqueness: {scope: :stree_name}
+    # validate :duplicate_address
     
 
     def avgScore
         return 0 unless comments.count.positive?
         # comments.average(:rating_noise, :rating_staff, :rating_safety, :rating_maintenance, :rating_appearance).round(2).to_f
-        comments.average(:rating).round(2).to_f
+        # comments.average(:rating).round(2).to_f
     end
 
     def avgScore_noise
@@ -49,5 +49,15 @@ class Apartment < ApplicationRecord
         end
         return collection
     end
+
+    # def duplicate_address
+    #     addresses = Apartment.map do |address|
+    #         `#{address.full_address}`
+    #     end
+    #     byebug
+    #     if addresses.include?(`#{self.street_number} #{self.street_name}, #{self.city}, #{self.state}`)
+    #         errors.add(:full_address, "Already exists.")
+    #     end
+    # end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -15,14 +15,13 @@ User.create(username: 'Roy_nyc', password: '321', first_name: 'Roy', last_name: 
 User.create(username: 'Snowboarder85', password: '789', first_name: 'Jordan', last_name: 'Kirsch')
 User.create(username: 'JeepAllTheWay', password: '987', first_name: 'Chelsea', last_name: 'Ritz-Kirsch')
 
-5.times do
+
 Apartment.create(street_number: 1, street_name: "River Place", city: "New York", state: "NY", zipcode: 10036, country: "USA", photoImg: "https://cdngeneral.rentcafe.com/dmslivecafe/3/228633/11-Waverly-Place--Front_WEB.jpg?crop=(0,0,300,225)&cropxunits=300&cropyunits=225&width=350")
 Apartment.create(street_number: 415, street_name: "West 57th street", city: "New York", state: "NY", zipcode: 10031, country: "USA", photoImg: "https://media.guestofaguest.com/t_article_content/gofg-media/2019/01/1/51893/49933486_107361643707543_4299262240395492308_n.jpg")
 Apartment.create(street_number: 1793, street_name: "Amsterdam Avenue", city: "New York", state: "NY", zipcode: 10031, country: "USA", photoImg: "https://mymagicearth.com/wp-content/uploads/2019/04/Mormandie-Apartments_1.jpg")
-end
 
-5.times do
-Comment.create(message: "This is a great place!", rating: rand(1..5), rating_noise: rand(1..5), rating_appearance: rand(1..5), rating_safety: rand(1..5), rating_staff: rand(1..5), rating_maintenance: rand(1..5), user_id: User.all.sample.id, apartment_id: Apartment.all.sample.id)
-Comment.create(message: "Terrible place!", rating: rand(1..5), rating_noise: rand(1..5), rating_appearance: rand(1..5), rating_safety: rand(1..5), rating_staff: rand(1..5), rating_maintenance: rand(1..5), user_id: User.all.sample.id, apartment_id: Apartment.all.sample.id)
-Comment.create(message: "This is a an ok place", rating: rand(1..5), rating_noise: rand(1..5), rating_appearance: rand(1..5), rating_safety: rand(1..5), rating_staff: rand(1..5), rating_maintenance: rand(1..5), user_id: User.all.sample.id, apartment_id: Apartment.all.sample.id)
-end
+
+
+# Comment.create(message: "This is a great place!", rating: rand(1..5), rating_noise: rand(1..5), rating_appearance: rand(1..5), rating_safety: rand(1..5), rating_staff: rand(1..5), rating_maintenance: rand(1..5), user_id: User.all.sample.id, apartment_id: Apartment.all.sample.id)
+# Comment.create(message: "Terrible place!", rating: rand(1..5), rating_noise: rand(1..5), rating_appearance: rand(1..5), rating_safety: rand(1..5), rating_staff: rand(1..5), rating_maintenance: rand(1..5), user_id: User.all.sample.id, apartment_id: Apartment.all.sample.id)
+# Comment.create(message: "This is a an ok place", rating: rand(1..5), rating_noise: rand(1..5), rating_appearance: rand(1..5), rating_safety: rand(1..5), rating_staff: rand(1..5), rating_maintenance: rand(1..5), user_id: User.all.sample.id, apartment_id: Apartment.all.sample.id)
