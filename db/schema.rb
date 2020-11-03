65ae4cc3ce82c9006582adbe8f93d3fd1c3dd517
David-Kirsch <davidjkirsch@gmail.com>
2020-11-03 13:49:36 -0500

    added verified for apartment listing creation

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_03_045921) do
+ActiveRecord::Schema.define(version: 2020_11_03_184733) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 2020_11_03_045921) do
     t.integer "zipcode"
     t.float "lat"
     t.float "lng"
+    t.string "verified"
   end
 
   create_table "comments", force: :cascade do |t|
